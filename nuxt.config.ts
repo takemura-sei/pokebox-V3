b510eb01a375f107bc814662eaa854cf286ecdfa
takemura-sei <seikun1127@gmail.com>
2024-12-08 18:19:19 +0900

    ✨piniaの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
 })
