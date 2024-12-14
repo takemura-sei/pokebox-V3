5b86a5f2322729e3fc31784cf2d7dbf98a7aabf6
takemura-sei <seikun1127@gmail.com>
2024-12-14 09:43:25 +0900

    ✨pluginsディレクトリにpokeAPI取得のaxios設定を追加 📚ドキュメントの追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+  const { $pokeApi } = useNuxtApp();
+
+  const fetchData = async () => {
+    await $pokeApi.get('pokemon')
+    .then(response => {
+      console.log(response.data)
+    })
+  }
+
+  const pokemonData = async () => {
+    const res = await $pokeApi.get('ability');
+    console.log(res.data);
+  }
+
+  fetchData();
+  pokemonData();
+</script>
+
 <template>
   <h1 class="bg-blue-500">
     Hello World!
