90b728746791df84a664e848b9356f03fd85dc1f
takemura-sei <seikun1127@gmail.com>
2025-01-30 07:42:04 +0900

    お気に入りポケモンには星を表示する機能の追加

--- src/components/card/PokemonCard.vue
+++ src/components/card/PokemonCard.vue
@@ -3,7 +3,10 @@
 import PokemonImages from '@/components/card/PokemonImages.vue';
 import PokemonJpName from '@/components/card/PokemonJpName.vue';
 import Modal from '@/components/modal/ Modal .vue';
+import { usePokemonDataStoreV2 } from '@/stores/pokemonDataStore_V2';
 import useModal from '@/composables/useModal';
+
+const usePokemonDataStore = usePokemonDataStoreV2();
 
 const { isModalOpen, openModal, closeModal } = useModal();
 
@@ -17,10 +20,37 @@ const props = defineProps({
 
 <template>
   <div class="bg-white p-2 rounded-lg" @click="openModal()">
-    <PokemonJpName :name="props.data.name" :url="props.data.url"/>
-    <PokemonImages :name="props.data.name" :url="props.data.url"/>
+    <div class="flex">
+      <PokemonJpName :name="data.name" :url="data.url"/>
+      <svg
+        v-if="usePokemonDataStore.favoriteBox[data.name]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="favorite-icon favorite-active"
+      >
+        <path
+          fill="currentColor"
+          d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+        />
+      </svg>
+    </div>
+    <PokemonImages :name="data.name" :url="data.url"/>
 
     <!-- モーダルウィンドウの表示 -->
-    <Modal v-if="isModalOpen" :name="props.data.name" :url="props.data.url" @close="closeModal()"/>
+    <Modal v-if="isModalOpen" :name="data.name" :url="data.url" @close="closeModal()"/>
   </div>
 </template>
+
+<style scoped>
+/* 星アイコンのサイズ調整 */
+.favorite-icon {
+  width: 24px;
+  height: 24px;
+  transition: color 0.3s ease;
+}
+
+/* 登録済みの場合の色 (黄色) */
+.favorite-active {
+  color: #ffc107;
+}
+</style>
--- src/components/favorite/FavoritePokemon.vue
+++ src/components/favorite/FavoritePokemon.vue
@@ -27,10 +27,10 @@ const isFavorite = computed(() => !!pokemonDataStore.favoriteBox[props.name])
       class="favorite-icon"
       :class="{ 'favorite-active': isFavorite, 'favorite-inactive': !isFavorite }"
     >
-    <path
-      fill="currentColor"
-      d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-    />
+      <path
+        fill="currentColor"
+        d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+      />
     </svg>
   </button>
 </template>
